993b3ba8912f625250c4b2098cd006e17a1fa613
NandoTheessen <nando.theessen@gmail.com>
2018-07-07 23:22:03 +0200

    changed resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -10,10 +10,8 @@ const Resume = () => {
                 <h6 className='resume-item-title'>N&N Cryptomining UG, <span className='font-weight-bold'>CEO & Founder</span></h6>
                 <p className="resume-item-subtitle font-italic">The N&N Cryptomining aimed to consult for big cryptomining projects & provide hardware</p>
                 <ul className='resume-item-list'>
-                    <li className="resume-item-list-item">Helped build an 800.000$ project in Bosnia, providing know-how & support</li>
+                    <li className="resume-item-list-item">Helped build a project of 800.000$ in Bosnia, providing know-how & support</li>
                     <li className="resume-item-list-item">Shipped custom built & configured mining hardware to customers</li>
-                    <li className="resume-item-list-item">Led my campaign to huge success raising over 7 million € for NPOs like Amnesty International,<br />
-                        SOS Kinderdörfer and the WWF</li>
                 </ul>
             </div>
             <div className='resume-item previous-experience'>
