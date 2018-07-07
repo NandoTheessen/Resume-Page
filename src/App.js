ce5319e90ad76d77ad1ea70a59f3dc497595397d
NandoTheessen <nando.theessen@gmail.com>
2018-07-07 13:29:25 +0200

    Added Resume component, added styling to the component, integrated the component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom';
 import Content from "./components/Content";
 import Portfolio from './components/Portfolio';
 import Footer from './components/Footer';
+import Resume from './components/Resume';
 
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
         <Route path='/' component={Header} />
         <Route exact path='/' component={Content} />
         <Route exact path='/portfolio' component={Portfolio} />
+        <Route exact path='/resume' component={Resume} />
         <Route path='/' component={Footer} />
       </div>
     );
