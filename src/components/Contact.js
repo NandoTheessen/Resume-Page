3621f2514f6f6062106dd52c323c79224b5b550b
NandoTheessen <nando.theessen@gmail.com>
2018-07-08 23:34:05 +0200

    Styling changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,8 +7,8 @@ const Contact = () => {
             <h5 className="contact-title">Please feel free to leave a message!</h5>
             <form className='contact-form' action="https://formspree.io/nando.theessen@gmail.com"
                 method="POST">
-                <input placeholder='E-Mail' className='form-element' type="email" id='mail' name="_replyto" />
                 <input placeholder='Name' className='form-element' type="text" id='name' name="name" />
+                <input placeholder='E-Mail' className='form-element' type="email" id='mail' name="_replyto" />
                 <textarea placeholder='Message' className='form-element' id='msg' type="text" name="message" />
                 <input id='button' type="submit" value="Send" />
             </form>
