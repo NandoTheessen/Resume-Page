8419f237a2a095dbfd9d70819203e9bb633eed75
NandoTheessen <nando.theessen@gmail.com>
2018-07-10 01:17:59 +0200

    converted header into stateful component to control the side menu for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css'
 import { stack as Menu } from 'react-burger-menu'
 
-const Header = (props) => {
 
-    return (
-        <div className='header'>
-            <Link to='/'><h1>NANDO THEESSEN</h1></Link>
-            <Menu right width={192}>
-                <Link className="menu-item" to='/portfolio'><i className="fas fa-project-diagram"></i><span>PORTFOLIO</span></Link>
-                <Link className="menu-item" to='/resume'><i class="far fa-file"></i><span>RESUME</span></Link>
-                <Link className="menu-item" to='/publications'><i class="fas fa-flask"></i><span>PUBLICATIONS</span></Link>
-                <Link className="menu-item" to='/education'><i class="fas fa-graduation-cap"></i><span>EDUCATION</span></Link>
-                <Link className="menu-item" to='/skills'><i class="fas fa-cogs"></i><span>SKILLS</span></Link>
-                <Link className="menu-item" to='/contact'><i class="far fa-envelope-open"></i><span>CONTACT</span></Link>
-            </Menu>
-            <nav className='over768' >
-                <Link className='navLink' to='/portfolio'><span>PORTFOLIO</span></Link>
-                <Link className='navLink' to='/resume'><span>RESUME</span></Link>
-                <Link className='navLink' to='/publications'><span>PUBLICATIONS</span></Link>
-                <Link className='navLink' to='/education'><span>EDUCATION</span></Link>
-                <Link className='navLink' to='/skills'><span>SKILLS</span></Link>
-                <Link className='navLink' to='/contact'><span>CONTACT</span></Link>
-            </nav>
-        </div>
-    );
-};
-
-
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuOpen: false
+        }
+    }
+    handleStateChange(state) {
+        this.setState({ menuOpen: state.menuOpen })
+    }
+    closeMenu() {
+        this.setState({ menuOpen: false })
+    }
+    toggleMenu() {
+        this.setState({ menuOpen: !this.state.menuOpen })
+    }
+    render() {
+        return (
+            <div className='header'>
+                <Link to='/'><h1>NANDO THEESSEN</h1></Link>
+                <Menu right width={192}
+                    isOpen={this.state.menuOpen}
+                    onStateChange={(state) => this.handleStateChange(state)}
+                >
+                    <Link className="menu-item" onClick={() => this.closeMenu()} to='/portfolio'><i className="fas fa-project-diagram"></i><span>PORTFOLIO</span></Link>
+                    <Link className="menu-item" onClick={() => this.closeMenu()} to='/resume'><i class="far fa-file"></i><span>RESUME</span></Link>
+                    <Link className="menu-item" onClick={() => this.closeMenu()} to='/publications'><i class="fas fa-flask"></i><span>PUBLICATIONS</span></Link>
+                    <Link className="menu-item" onClick={() => this.closeMenu()} to='/education'><i class="fas fa-graduation-cap"></i><span>EDUCATION</span></Link>
+                    <Link className="menu-item" onClick={() => this.closeMenu()} to='/skills'><i class="fas fa-cogs"></i><span>SKILLS</span></Link>
+                    <Link className="menu-item" onClick={() => this.closeMenu()} to='/contact'><i class="far fa-envelope-open"></i><span>CONTACT</span></Link>
+                </Menu>
+                <nav className='over768' >
+                    <Link className='navLink' to='/portfolio'><span>PORTFOLIO</span></Link>
+                    <Link className='navLink' to='/resume'><span>RESUME</span></Link>
+                    <Link className='navLink' to='/publications'><span>PUBLICATIONS</span></Link>
+                    <Link className='navLink' to='/education'><span>EDUCATION</span></Link>
+                    <Link className='navLink' to='/skills'><span>SKILLS</span></Link>
+                    <Link className='navLink' to='/contact'><span>CONTACT</span></Link>
+                </nav>
+            </div>
+        );
+    }
+}
 
 export default Header;
