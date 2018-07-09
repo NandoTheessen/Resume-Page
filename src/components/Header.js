1cc00fe040905c81176f2e9c900aee60875e72b5
NandoTheessen <nando.theessen@gmail.com>
2018-07-10 00:22:27 +0200

    replaced select menu w/ hamburger style menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css'
+import { stack as Menu } from 'react-burger-menu'
 
 const Header = (props) => {
-    function onChange(e) {
-        props.history.push(`/${e.currentTarget.value}`)
-    }
+
     return (
         <div className='header'>
             <Link to='/'><h1>NANDO THEESSEN</h1></Link>
-
-            <nav>
+            <Menu right width={192}>
+                <Link className="menu-item" to='/portfolio'><i className="fas fa-project-diagram"></i><span>PORTFOLIO</span></Link>
+                <Link className="menu-item" to='/resume'><i class="far fa-file"></i><span>RESUME</span></Link>
+                <Link className="menu-item" to='/publications'><i class="fas fa-flask"></i><span>PUBLICATIONS</span></Link>
+                <Link className="menu-item" to='/education'><i class="fas fa-graduation-cap"></i><span>EDUCATION</span></Link>
+                <Link className="menu-item" to='/skills'><i class="fas fa-cogs"></i><span>SKILLS</span></Link>
+                <Link className="menu-item" to='/contact'><i class="far fa-envelope-open"></i><span>CONTACT</span></Link>
+            </Menu>
+            <nav className='over768' >
                 <Link className='navLink' to='/portfolio'><span>PORTFOLIO</span></Link>
                 <Link className='navLink' to='/resume'><span>RESUME</span></Link>
                 <Link className='navLink' to='/publications'><span>PUBLICATIONS</span></Link>
                 <Link className='navLink' to='/education'><span>EDUCATION</span></Link>
                 <Link className='navLink' to='/skills'><span>SKILLS</span></Link>
                 <Link className='navLink' to='/contact'><span>CONTACT</span></Link>
-
-                <select onChange={onChange} defaultValue='Navigation'>
-                    <option value="">Navigation</option>
-
-                    <option className='option-item' value="Portfolio">Portfolio</option>
-                    <option className='option-item' value="Resume">Resume</option>
-                    <option className='option-item' value="Publications">Publications</option>
-                    <option className='option-item' value="Education">Education</option>
-                    <option className='option-item' value="Skills">Skills</option>
-                    <option className='option-item' value="Contact">Contact</option>
-                </select>
             </nav>
         </div>
     );
