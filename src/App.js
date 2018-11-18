fe12b3bdadebbdd5c77d08d00c243ddc0bfba3fd
NandoTheessen <nando.theessen@gmail.com>
2018-11-18 02:23:55 +0100

    Create SocialContainer component: - Add data for social platforms - Add FA svg icons for social platforms - Display links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import HeaderContainer from './components/Header';
 import { Route } from 'react-router-dom';
+import Social from './components/Social';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Route path="/" component={HeaderContainer} />
+        <Social />
         {/*         <Route exact path="/" component={Content} />
         <Route path="/" component={Footer} /> */}
       </div>
--- src/components/Social.js
+++ src/components/Social.js
@@ -1,22 +1,59 @@
 import React from 'react';
+import styled from 'react-emotion';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faTwitter,
+  faMedium,
+  faLinkedin
+} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-const Social = styled('div')``;
+const Social = styled('div')`
+  a {
+    text-decoration: none;
+  }
+  a:hover {
+    color: white;
+  }
+`;
 
 const data = [
   {
     name: 'Twitter',
-    url: 'https://twitter.com/NandoTCodes'
+    url: 'https://twitter.com/NandoTCodes',
+    logo: faTwitter
   },
   {
     name: 'Email',
-    url: 'mailto:Nando.Theessen@gmail.com'
+    url: 'mailto:Nando.Theessen@gmail.com',
+    logo: faEnvelope
   },
   {
     name: 'medium',
-    url: 'https://medium.com/@nando.theessen'
+    url: 'https://medium.com/@nando.theessen',
+    logo: faMedium
+  },
+  {
+    name: 'linkedIn',
+    url: 'https://www.linkedin.com/in/nandotheessen/?locale=en_US',
+    logo: faLinkedin
   }
 ];
 
 const SocialContainer = props => {
-  <Social>Links</Social>;
+  return (
+    <Social>
+      {data.map(e => {
+        return (
+          <Link key={e.name} to={e.url}>
+            <FontAwesomeIcon icon={e.logo} />
+            {e.name}
+          </Link>
+        );
+      })}
+    </Social>
+  );
 };
+
+export default SocialContainer;
